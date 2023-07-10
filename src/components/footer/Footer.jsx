80c1df165abb428e9d15dd3374e478cfd128716c
guifeel <guifeel@qq.com>
2023-07-10 18:28:48 +0800

    footer组件

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        雷诺
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://game.midgoogle.top">哎呦，可以哦～</a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy;React Portfolio. All rights reserved</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
